c4c8796ccecb0731aa8cd14a46b7eead2b41a471
Yvo <akira.cn@gmail.com>
2023-09-22 16:46:47 +0800

    Update via AirCode on Sep 22, 2023

--- examples/stable-diffusion-huggingface/index.mjs
+++ examples/stable-diffusion-huggingface/index.mjs
@@ -9,24 +9,51 @@ const headers = {
   Authorization: `Bearer ${process.env.API_TOKEN}`,
 };
 
+const fileTable = aircode.db.table('_files');
+
 export default async function (params, context) {
-  const {caption, direct_output} = params;
+  let {caption, seed, direct_output} = params;
+  seed = seed || Math.random();
+
+  if(direct_output) context.set('Content-type', 'image/png');
+
+  const cache = await fileTable.where({caption, seed}).findOne();
+  if(cache) {
+    if(direct_output) {
+      const content = await aircode.files.download({
+        _id: cache._id
+      }, {
+        dataType: 'buffer',
+      });
+      return content;
+    }
+    return {fromCache: true, file: cache};
+  }
+  
   const response = await fetch(API_URL, {
     headers,
     method: "POST",
-    body: JSON.stringify({inputs: caption, seed: Math.random()}),
+    body: JSON.stringify({inputs: caption, seed}),
   });
+
   const result = await response.arrayBuffer();
   const buffer = Buffer.from(new Uint8Array(result));
 
+  const uploadTask = aircode.files.upload(
+    buffer,
+    `${caption.replace(/\s/g,'-').slice(0, 24)}.png`,
+    {
+      additions: {
+        caption,
+        seed,
+      }
+    }
+  );
+
   if(direct_output) {
-    context.set('Content-type', 'image/png');
     return buffer;
   } else {
-    const file = await aircode.files.upload(
-      buffer,
-      `${caption.replace(/\s/g,'-').slice(0, 24)}.png`
-    );
+    const file = await uploadTask;
     return {file};
   }
 };
